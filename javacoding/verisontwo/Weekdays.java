0716d39990cdbe3569e910bcca2b2baf7340ce4a
zhangxianlei <zhangxianlei@sungrowpower.com>
2022-08-07 17:07:27 +0800

    feat: constructor of enum

--- javacoding/verisontwo/Weekdays.java
+++ javacoding/verisontwo/Weekdays.java
@@ -1,14 +1,19 @@
 package javacoding.verisontwo;
 
 public enum Weekdays {
-  MONDAY, TUESDAY, WEDENSDAY, THURSDAY, FRIDAY;
+  MONDAY("星期一", 0), TUESDAY("星期二", 1), WEDENSDAY("星期三", 2), THURSDAY("星期四", 3), FRIDAY("星期五", 4);
 
-  private String name = "zxl";
-  private int index = 199;
+  private String name;
+  private int index;
   public static int sum = 9999;
 
   private static int staticVar = 100;
   public int buga = 99990;
+
+  private Weekdays(String name, int index) {
+    this.name = name;
+    this.index = index;
+  }
 
   @Override
   public String toString() {
