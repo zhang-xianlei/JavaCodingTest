a546158fbb3c09d0e0d540dc7329ff5c01bc88ba
zhangxianlei <zhangxianlei@sungrowpower.com>
2022-09-21 00:02:12 +0800

    feat: add stack pra

---
+++ javacoding/src/com/sungrowCharger/Stack.java
@@ -0,0 +1,27 @@
+package javacoding.src.com.sungrowCharger;
+
+class Stack {
+    private int pointer = 5;
+    private char[] data = new char[5];
+    public synchronized void push(char c){
+        while(pointer == data.length){
+            try{
+                this.wait();
+            } catch (InterruptedException e){
+            }
+        }
+        this.notify();
+        data[pointer] = c;
+        pointer++;
+    }
+    public synchronized char pop(){
+        while(pointer == 0){
+            try{
+                this.wait();
+            } catch(InterruptedException e){}
+        }
+        this.notify();
+        pointer--;
+        return data[pointer];
+    }
+}
