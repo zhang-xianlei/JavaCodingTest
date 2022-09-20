9130132ebaf7857e520457c19d7eab0811372cf2
zhangxianlei <zhangxianlei@sungrowpower.com>
2022-09-20 23:20:15 +0800

    feat: synchronized pra

---
+++ javacoding/src/com/sungrowCharger/HandleSellSyncRefactor.java
@@ -0,0 +1,49 @@
+package javacoding.src.com.sungrowCharger;
+
+public class HandleSellSyncRefactor {
+    public static void main(String[] args){
+        TicketDB db = new TicketDB();
+        Thread t1 = new Thread(() -> {
+            while(true){
+                if(!job(db)){
+                    break;
+                }
+                try{
+                    Thread.sleep(1000);
+                } catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "Ticket lobby one");
+        t1.start();
+
+        Thread t2 = new Thread(() -> {
+            while(true){
+                if(!job(db)){
+                    break;
+                }
+                try{
+                    Thread.sleep(1000);
+                } catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }, "Ticket lobby two");
+        t2.start();
+    }
+
+    private static boolean job(TicketDB db){
+        synchronized(db){
+            int currTicketAccount = db.getTicketCount();
+            if(currTicketAccount > 0) {
+                db.sellTicket();
+            } else {
+                return false;
+            }
+            String threadName = Thread.currentThread().getName();
+            System.out.printf("%S -- selling the %d ticket. \n", threadName, currTicketAccount);
+            return true;
+        }
+        
+    }
+}
